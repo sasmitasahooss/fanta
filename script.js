8f1e46a5767b4e85b09aa5fbdb6ac4ba95df88b5
sasmitasahooss <sahoosasmita5420@gmail.com>
2024-11-29 12:24:51 +0530

    done

---
+++ script.js
@@ -0,0 +1,57 @@
+var tl=gsap.timeline({scrollTrigger:{
+    trigger: ".two",
+    start: "0% 95%",
+    end:"50% 50%",
+    scrub: true,
+    // markers: true
+}})
+tl.to("#fanta",{
+    top:"120%",
+    left:"0%"
+}, 'orange')
+tl.to("#orange-cut",{
+    top:"160%",
+    left:"23%",
+},'orange')
+tl.to("#orange",{
+    top:"170%",
+    right:"6%",
+    width:"15%"
+},'orange')
+tl.to("#leaf",{
+    top:"110%",
+    left:"80%",
+    rotate:"250deg",
+    width:"13%"
+},'orange')
+var tl2=gsap.timeline({scrollTrigger:{
+    trigger: ".three",
+    start: "0% 95%",
+    end:"20% 50%",
+    scrub: true,
+    markers: true
+}})
+tl2.from(".lemon1",{
+    rotate:"-90deg",
+    left:"-100%",
+    top:"110%",
+
+},'ca')
+tl2.from("#cocacola",{
+    rotate:"-90deg",
+    left:"-100%",
+    top:"110%",
+
+},'ca')
+tl2.from(".lemon2",{
+    rotate:"90deg",
+    left:"80%",
+    top:"110%",
+
+},'ca')
+tl2.from("#pepsi",{
+    rotate:"90deg",
+    left:"80%",
+    top:"110%",
+
+},'ca')
